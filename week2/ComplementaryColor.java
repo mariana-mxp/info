e1fe4b37a37e32fd40e0c13af8d520b4e6a09c6e
mariana-mxp <mxp@ecomp.poli.br>
2016-03-16 14:13:21 -0300

    Insercao da lingua francesa

--- week2/ComplementaryColor.java
+++ week2/ComplementaryColor.java
@@ -2,7 +2,7 @@
 
 public class ComplementaryColor {
 	
-	
+
 	   public String representationColorPortuguese(String userColor){
 	   	String colorRepresentation="";
 	   	
@@ -24,7 +24,7 @@ public class ComplementaryColor {
 		 	else if (userColor.equals("laranja")) {
 		 		colorRepresentation = "tangerina";
 		 	};
-			
+
 			return colorRepresentation;
 	   }//end representationColorPortuguese
 
@@ -104,6 +104,57 @@ public class ComplementaryColor {
 			return complementaryColor;
 	   }//end complementaryColorEnglish
 
+	    ///****////
+	   public String representationColorFrench(String userColor){
+	   	String colorRepresentation="";
+	   	
+	   	if (userColor.equals("rouge")) {
+		 	colorRepresentation = "tomate";
+		 }
+		 	else if (userColor.equals("pourpre")) {
+				colorRepresentation = "aubergine";
+			}
+			else if (userColor.equals("bleu")) {
+				colorRepresentation = "blueberry";
+			}
+			else if (userColor.equals("vert")) {
+				colorRepresentation = "salade";
+			}
+			else if (userColor.equals("jaune")) {
+		 		colorRepresentation = "melon";
+		 	}
+		 	else if (userColor.equals("orange")) {
+		 		colorRepresentation = "mandarine";
+		 	};
+			
+			return colorRepresentation;
+	   }//end representationColorFrench
+
+	    public String complementaryColorFrench(String userColor){
+	   	String complementaryColor="";
+	   	
+	   	if (userColor.equals("rouge")) {
+		 	complementaryColor = "vert";
+		 }
+		 	else if (userColor.equals("pourpre")) {
+				complementaryColor = "jaune";
+			}
+			else if (userColor.equals("bleu")) {
+				complementaryColor = "orange";
+			}
+			else if (userColor.equals("vert")) {
+				complementaryColor = "rouge";
+			}
+			else if (userColor.equals("jaune")) {
+		 		complementaryColor = "pourpre";
+		 	}
+		 	else if (userColor.equals("orange")) {
+		 		complementaryColor = "bleu";
+		 	};
+			
+			return complementaryColor;
+	   }//end complementaryColorFrench
+
 	
 	/* This main method receives two arguments: language and color.
 	   Thus, you need to provide them when you're going to execute it.
@@ -121,6 +172,7 @@ public class ComplementaryColor {
 	 * 
 	 * @param args[0] Portuguese (pt)
 	 		  args[0] English (en)
+	 		  args[0] French (fr)
 
 	 * @param args[1] color (Ex: vermelho)		
 	 */
@@ -135,24 +187,48 @@ public class ComplementaryColor {
 		String representationUserColor = "";
 		//cor complementar
 		String complementaryUserColor = "";
-		
 
 		switch(language){
 			case "pt":
 				representationUserColor = complementaryColor.representationColorPortuguese(color);
 				complementaryUserColor = complementaryColor.complementaryColorPortuguese(color);
-				System.out.println("Cor de " + representationUserColor + ".");
-				System.out.println("Sua cor complementar é: " + complementaryUserColor + "!");
+				if (color.equals("vermelho")||color.equals("roxo")||color.equals("azul")||color.equals("verde")||color.equals("amarelo")
+					||color.equals("laranja")){
+					System.out.println("Cor de " + representationUserColor + ".");
+					System.out.println("Sua cor complementar é: " + complementaryUserColor + "!");	
+				} else {  
+					System.out.println("Favor selecionar uma das cores disponíveis: vermelho, roxo, azul, verde, amarelo ou laranja. ");	
+				}
 				break;
 				
 			case "en":
 				representationUserColor = complementaryColor.representationColorEnglish(color);
 				complementaryUserColor = complementaryColor.complementaryColorEnglish(color);
+				if (color.equals("red")||color.equals("purple")||color.equals("blue")||color.equals("green")||color.equals("yellow")
+					||color.equals("orange")){
 				System.out.println(representationUserColor + " color.");
 				System.out.println("Your complementary color is: " + complementaryUserColor + "!");
+				} else {  
+					System.out.println("Please select one of the available colors: red, purple, blue, green, yellow or orange.");	
+				}
 				break;
 				
+			case "fr":
+				representationUserColor = complementaryColor.representationColorFrench(color);
+				complementaryUserColor = complementaryColor.complementaryColorFrench(color);
+				if (color.equals("rouge")||color.equals("pourpre")||color.equals("bleu")||color.equals("vert")||color.equals("jaune")
+					||color.equals("orange")){
+				System.out.println("Couleur " + representationUserColor + ".");
+				System.out.println("Sa couleur complémentaire est: " + complementaryUserColor + "!");
+				} else {  
+					System.out.println("S'il vous plaît sélectionnez une des couleurs disponibles: rouge, violet, bleu, vert, jaune ou orange.");	
+				}
+				break;
+
 			default:
+				System.out.println("Por favor escolha uma das linguagens aceitas: pt (Português), en (Inglês) ou fr (Francês).");
+				System.out.println("Please choose one of the accepted languages pt (Portuguese), en (English) or fr (French).");
+				System.out.println("S'il vous plaît choisissez l'une des langages supportés: pt (Portugais), en (Anglais) ou fr (Français).");
 				break;
 				
 			
