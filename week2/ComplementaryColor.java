67829f8c7ba0462591b5eac2675994ed30414d2e
mariana-mxp <mxp@ecomp.poli.br>
2016-03-16 13:01:15 -0300

    Insercao da lingua inglesa

--- week2/ComplementaryColor.java
+++ week2/ComplementaryColor.java
@@ -2,54 +2,162 @@
 
 public class ComplementaryColor {
 	
+	
+	   public String representationColorPortuguese(String userColor){
+	   	String colorRepresentation="";
+	   	
+	   	if (userColor.equals("vermelho")) {
+		 	colorRepresentation = "tomate";
+		 }
+		 	else if (userColor.equals("roxo")) {
+				colorRepresentation = "berinjela";
+			}
+			else if (userColor.equals("azul")) {
+				colorRepresentation = "mirtilo";
+			}
+			else if (userColor.equals("verde")) {
+				colorRepresentation = "alface";
+			}
+			else if (userColor.equals("amarelo")) {
+		 		colorRepresentation = "melão";
+		 	}
+		 	else if (userColor.equals("laranja")) {
+		 		colorRepresentation = "tangerina";
+		 	};
+			
+			return colorRepresentation;
+	   }//end representationColorPortuguese
+
+	    public String complementaryColorPortuguese(String userColor){
+	   	String complementaryColor="";
+	   	
+	   	if (userColor.equals("vermelho")) {
+		 	complementaryColor = "verde";
+		 }
+		 	else if (userColor.equals("roxo")) {
+				complementaryColor = "amarelo";
+			}
+			else if (userColor.equals("azul")) {
+				complementaryColor = "laranja";
+			}
+			else if (userColor.equals("verde")) {
+				complementaryColor = "vermelho";
+			}
+			else if (userColor.equals("amarelo")) {
+		 		complementaryColor = "roxo";
+		 	}
+		 	else if (userColor.equals("laranja")) {
+		 		complementaryColor = "azul";
+		 	};
+			
+			return complementaryColor;
+	   }//end complementaryColorPortuguese
+
+	   ///****////
+	   public String representationColorEnglish(String userColor){
+	   	String colorRepresentation="";
+	   	
+	   	if (userColor.equals("red")) {
+		 	colorRepresentation = "Tomato";
+		 }
+		 	else if (userColor.equals("purple")) {
+				colorRepresentation = "Eggplant";
+			}
+			else if (userColor.equals("blue")) {
+				colorRepresentation = "Blueberry";
+			}
+			else if (userColor.equals("green")) {
+				colorRepresentation = "Lettuce";
+			}
+			else if (userColor.equals("yellow")) {
+		 		colorRepresentation = "Melon";
+		 	}
+		 	else if (userColor.equals("orange")) {
+		 		colorRepresentation = "Tangerine";
+		 	};
+			
+			return colorRepresentation;
+	   }//end representationColorEnglish
+
+	    public String complementaryColorEnglish(String userColor){
+	   	String complementaryColor="";
+	   	
+	   	if (userColor.equals("red")) {
+		 	complementaryColor = "green";
+		 }
+		 	else if (userColor.equals("purple")) {
+				complementaryColor = "yellow";
+			}
+			else if (userColor.equals("blue")) {
+				complementaryColor = "orange";
+			}
+			else if (userColor.equals("green")) {
+				complementaryColor = "red";
+			}
+			else if (userColor.equals("yellow")) {
+		 		complementaryColor = "purple";
+		 	}
+		 	else if (userColor.equals("orange")) {
+		 		complementaryColor = "blue";
+		 	};
+			
+			return complementaryColor;
+	   }//end complementaryColorEnglish
+
+	
 	/* This main method receives two arguments: language and color.
 	   Thus, you need to provide them when you're going to execute it.
 	   For instance, if you run it on command line, you
-	   could simply run: "java ComplementaryColor pt vermelho"
+	   could simply run: "java ComplementaryColor pt vermelho" or 
+	   "java ComplementaryColor en red".
 	  
 	   If you're running on Eclipse, you need to set the arguments
 	   in "Run/Run configurations/Arguments/Program arguments"
 	   otherwise you'll just get a runtime exception and nothing will happen
 	   Needing help with that don't hesitate on contacting me
-	 */	
+	 */
+
 	/**
 	 * 
-	 * @param args[0] language (Ex: pt)
+	 * @param args[0] Portuguese (pt)
+	 		  args[0] English (en)
+
 	 * @param args[1] color (Ex: vermelho)		
 	 */
 	public static void main(String[] args) {
-		if (args[0].equals("pt")) {
-			String color = args[1];
-			System.out.print("Legal, você escolheu \"" + color);
-			String representation = "";
-			String cColor = "";
-			if ("vermelho".equals(color)) {
-				representation = "tomate";
-				cColor = "verde";
-			}
-			else if ("roxo".equals(color)) {
-				representation = "berinjela";
-				cColor = "amarelo";
-			}
-			else if ("azul".equals(color)) {
-				representation = "mirtilo";
-				cColor = "laranja";
-			}
-			else if ("verde".equals(color)) {
-				representation = "alface";
-				cColor = "vermelho";
-			}
-			else if ("amarelo".equals(color)) {
-				representation = "melão";
-				cColor = "roxo";
-			}
-			else if ("laranja".equals(color)) {
-				representation = "tangerina";
-				cColor = "azul";
-			};
-			System.out.println("\", cor de " + representation + ".");
+		ComplementaryColor complementaryColor = new ComplementaryColor();
 
-			System.out.println("Sua cor complementar é: " + cColor + "!");
-		}
+		//idioma do aplicativo
+		String language=args[0];
+		//cor escolhida pelo usuário
+		String color = args[1];
+		//representação da cor
+		String representationUserColor = "";
+		//cor complementar
+		String complementaryUserColor = "";
+		
+
+		switch(language){
+			case "pt":
+				representationUserColor = complementaryColor.representationColorPortuguese(color);
+				complementaryUserColor = complementaryColor.complementaryColorPortuguese(color);
+				System.out.println("Cor de " + representationUserColor + ".");
+				System.out.println("Sua cor complementar é: " + complementaryUserColor + "!");
+				break;
+				
+			case "en":
+				representationUserColor = complementaryColor.representationColorEnglish(color);
+				complementaryUserColor = complementaryColor.complementaryColorEnglish(color);
+				System.out.println(representationUserColor + " color.");
+				System.out.println("Your complementary color is: " + complementaryUserColor + "!");
+				break;
+				
+			default:
+				break;
+				
+			
+		}//end switch
+		
+			//System.out.println("COR: " + teste);
 	}
 }
